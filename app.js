3b91cc294d7acad768a60884b800393f1da71c97
Samuel.Cai <samuel.cai@ceosoftcenters.com>
2017-07-03 22:22:12 -0500

    make my csurf feature works.

--- app.js
+++ app.js
@@ -2,14 +2,18 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var session = require('express-session');
+var cookieParser = require('cookie-parser');
+
 var bodyParser = require('body-parser');
 var validator = require('express-validator');
 var lessMiddleware = require('less-middleware');
 
 var index = require('./routes/index');
 var newsletter = require('./routes/newletter');
+
+// for all request add csrf validation
+let csurf = require('csurf');
 
 var app = express();
 
@@ -19,26 +23,36 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 //application/x-www-form-urlencoded is the default mime-type of your request
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }} // !!!! if here I set cookie: { secure: true} ---- all post request will block. server response 403
+));
+/**
+ * https://github.com/expressjs/session
+ * Please note that secure: true is a recommended option. However, it requires an https-enabled website, i.e.,
+ * HTTPS is necessary for secure cookies. If secure is set, and you access your site over HTTP, the cookie will not be set.
+ * If you have your node.js behind a proxy and are using secure: true, you need to set "trust proxy" in express:
+ */
+
 app.use(validator());
 app.use(lessMiddleware(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(csurf());
+app.use(function(request, response, next){
+    response.locals.csrftoken = request.csrfToken();
+    console.log("Samuel Test csrftoken = " + request.csrfToken());
+    next();
+});
 
 app.use('/', index);
 app.use('/newsletter', newsletter);
-
-// for all request add csrf validation
-let csurf = require('csurf');
-app.use(csurf());
-app.use(function(request, response, next){
-  response.locals.csrftoken = request.csrftoken();
-  next();
-});
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/newletter.js
+++ routes/newletter.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 const bodyParser = require('body-parser');
-const urlencodeParser = bodyParser.urlencoded({ extended: false });
+const urlencodeParser = bodyParser.urlencoded({ extended: true });
 
 const {promisify} = require('util');
 const appendFilePro = promisify(fs.appendFile);
@@ -12,6 +12,7 @@ const Rx = require('@reactivex/rxjs');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
      //res.send('respond with a resource');
+     console.log("Samuel get request /");
      res.render('newsletter', {title: 'News Letter form'});
 });
 
@@ -27,13 +28,13 @@ router.post('/', urlencodeParser, function (req, res) {
     if (errors) res.render('error', {message: errors});
     else {
          // append email into subscribers.txt
-        fs.appendFile('subscribers.txt', req.body.email + '\n', function (err) {
-            if (err) throw err;
-            console.log('email appended!');
-
-            // redirect
-            res.render('thankyou', {email: req.body.email});
-        });
+        // fs.appendFile('subscribers.txt', req.body.email + '\n', function (err) {
+        //     if (err) throw err;
+        //     console.log('email appended!');
+        //
+        //     // redirect
+        //     res.render('thankyou', {email: req.body.email});
+        // });
 
         // extend1: try to use util.promise to make fs.appendFile() return one promise
         // appendFilePro('subscribers.txt', req.body.email + '\n')
@@ -41,25 +42,26 @@ router.post('/', urlencodeParser, function (req, res) {
         //     .catch(() => res.render('error', {message: [{msg:'append file error'}]}));
 
         // extend2: try to use Observable to do this
-        // let source = Rx.Observable.create((observer) => {
-        //     fs.appendFile('subscribers.txt', req.body.email + '\n', function (err) {
-        //             if (err) observer.error(err);
-        //
-        //             observer.next('sucessflag');
-        //
-        //             // why .complete() method not work.
-        //             observer.complete();
-        //     });
-        // });
-        //
-        // source.subscribe(function (flag) {
-        //     if (flag === 'sucessflag') {
-        //         console.log('email appended!');
-        //         // redirect
-        //         res.render('thankyou', {email: req.body.email});
-        //     }
-        // }, err => res.render('error', {message: [{msg:'subscrible email fail'}]},
-        // () => console.log('Done')));
+        let source = Rx.Observable.create((observer) => {
+            fs.appendFile('subscribers.txt', req.body.email + '\n', function (err) {
+                    if (err) observer.error(err);
+
+                    observer.next('sucessflag');
+
+                    // why .complete() method not work.?
+                    observer.complete();
+            });
+        });
+
+        source.subscribe(function (flag) {
+            if (flag === 'sucessflag') {
+                console.log('email appended!!!');
+                // redirect
+                res.render('thankyou', {email: req.body.email});
+            }
+        },  err => res.render('error', {message: [{msg:'subscrible email fail'}]}),
+            () => console.log('Done')
+        );
     }
 });
 
